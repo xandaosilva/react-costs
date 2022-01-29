ddb1cf5cb7e6ea90a4cbe8ccba17a5885b454cca
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-01-29 10:33:07 -0300

    Creating initial components and config routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+import NewProject from "./components/pages/NewProject";
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/company">
+          <Company />
+        </Route>
+        <Route exact path="/newproject">
+          <NewProject />
+        </Route>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   );
 }
 
