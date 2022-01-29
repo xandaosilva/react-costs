9722ba5b3f50de0205a6e73eb428f6a10f773bc8
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-01-29 10:54:01 -0300

    Creating component Container and container css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Container from "./components/layout/Container";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
@@ -14,18 +15,20 @@ function App() {
         <Link to="/newproject">New Project</Link>
       </div>
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-        <Route exact path="/company">
-          <Company />
-        </Route>
-        <Route exact path="/newproject">
-          <NewProject />
-        </Route>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/company">
+            <Company />
+          </Route>
+          <Route exact path="/newproject">
+            <NewProject />
+          </Route>
+        </Container>
       </Switch>
       <p>Footer</p>
     </Router>
