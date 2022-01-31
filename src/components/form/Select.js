ae5b4b1e6c7591c5348bc94e691942316456743c
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-01-31 16:37:18 -0300

    Conecting with API

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -6,6 +6,9 @@ export default function Select({ text, name, options, handleOnChange, value }){
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name}>
                 <option>Select the options</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>{option.name}</option>
+                ))}
             </select>
         </div>
     );
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 import styles from "./css/ProjectForm.module.css";
 
 export default function ProjectForm({ btnText }){
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application-json"
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => setCategories(data))
+        .catch((error) => console.log(error));
+    }, []);
+
     return(
         <form className={styles.form}>
             <Input type="text" text="Project's name" name="name" placeholder="Project's name"/>
             <Input type="number" text="Project's total budget" name="budget" placeholder="Project's total budget" />
-            <Select text="Select the category" name="category_id"/>
+            <Select text="Select the category" name="category_id" options={categories}/>
             <SubmitButton text={btnText}/>
         </form>
     );
