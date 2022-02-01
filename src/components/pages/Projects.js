01b86a353eaca2d5413d31f0174382dca5ab8b9a
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-02-01 15:55:52 -0300

    Loader component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,10 +5,12 @@ import styles from "./css/Projects.module.css"
 import LinkButton from "../layout/LinkButton";
 import ProjectCard from "../project/ProjectCard";
 import { useState, useEffect } from "react";
+import Loading from "../layout/Loading";
 
 export default function Projects(){
 
     const [projects, setProjects] = useState([]);
+    const [removeLoading, setRemoveLoading] = useState(false);
 
     const location = useLocation();
     let message = "";
@@ -18,18 +20,21 @@ export default function Projects(){
     }
 
     useEffect(() => {
-        fetch("http://localhost:5000/projects", {
-            method: "GET",
-            headers: {
-                "Content-Type" : "application/json",
-            }
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            console.log(data);
-            setProjects(data);
-        })
-        .catch((err) => console.log(err));
+        setTimeout(() => {
+            fetch("http://localhost:5000/projects", {
+                method: "GET",
+                headers: {
+                    "Content-Type" : "application/json",
+                }
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data);
+                setProjects(data);
+                setRemoveLoading(true);
+            })
+            .catch((err) => console.log(err)); 
+        }, 3000);
     }, []);
 
     return(
@@ -43,6 +48,8 @@ export default function Projects(){
                 {projects.length > 0 && projects.map((project) => (
                     <ProjectCard id={project.id} name={project.name} budget={project.budget} category={project.category.name} key={project.id}/>
                 ))}
+                {!removeLoading && <Loading />}
+                {removeLoading && projects.length === 0 && ( <p>No projects created</p> )}
             </Container>
         </div>
     );
